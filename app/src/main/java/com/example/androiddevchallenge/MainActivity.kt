2ec374ed5de5e43ace38b41adc15a9877e5460a5
yalematta <layale.matta@gmail.com>
2021-03-13 19:23:03 +0200

    implement welcome screen

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -1,33 +1,43 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.androiddevchallenge
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.intl.LocaleList
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             MyTheme {
                 MyApp()
@@ -36,11 +46,13 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-// Start building your app here!
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        WelcomePage(
+            onSignup = {},
+            onLogin = {}
+        )
     }
 }
 
@@ -52,10 +64,89 @@ fun LightPreview() {
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+@Preview("Dark Theme", widthDp = 360, heightDp = 640, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun DarkPreview() {
-    MyTheme(darkTheme = true) {
+    MyTheme {
         MyApp()
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomePagePreview() {
+    MyTheme {
+        Surface(Modifier.size(width = 360.dp, height = 640.dp)) {
+            WelcomePage({}, {})
+        }
+    }
+}
+
+@Composable
+fun WelcomePage(onSignup: () -> Unit, onLogin: () -> Unit) {
+    WelcomeBackground()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Logo()
+        Spacer(Modifier.height(32.dp))
+        BigButton(
+            onClick = onSignup,
+            backgroundColor = MaterialTheme.colors.primary
+        ) {
+            Text(stringResource(id = R.string.sign_up).toUpperCase(LocaleList.current))
+        }
+        Spacer(Modifier.height(8.dp))
+        BigButton(
+            onClick = onLogin,
+            backgroundColor = MaterialTheme.colors.secondary
+        ) {
+            Text(stringResource(id = R.string.log_in).toUpperCase(LocaleList.current))
+        }
+    }
+}
+
+@Composable
+fun WelcomeBackground() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_welcome),
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colors.background),
+        contentScale = ContentScale.FillBounds
+    )
+}
+
+@Composable
+fun Logo() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_logo),
+        contentDescription = stringResource(id = R.string.app_name)
+    )
+}
+
+@Composable
+fun BigButton(
+    onClick: () -> Unit,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit
+) {
+    Button(
+        onClick = onClick,
+        elevation = ButtonDefaults.elevation(0.dp, 0.dp, 0.dp),
+        shape = MaterialTheme.shapes.medium,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = backgroundColor
+        ),
+        modifier = modifier
+            .height(72.dp)
+            .fillMaxWidth(),
+        content = content
+    )
 }
