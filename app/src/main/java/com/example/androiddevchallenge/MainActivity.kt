a4998cc3021d86b781dc27e5cb6340d450c87efe
yalematta <layale.matta@gmail.com>
2021-03-13 19:59:56 +0200

    implement login screen

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -6,20 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,7 +16,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.intl.LocaleList
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -71,6 +65,8 @@ fun DarkPreview() {
         MyApp()
     }
 }
+
+// region Welcome Screen
 
 @Preview(showBackground = true)
 @Composable
@@ -150,3 +146,132 @@ fun BigButton(
         content = content
     )
 }
+// endregion
+
+// region Login Screen
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPagePreview() {
+    MyTheme {
+        Surface(Modifier.size(width = 360.dp, height = 640.dp)) {
+            LoginPage({}, {})
+        }
+    }
+}
+
+@Composable
+fun LoginPage(onLogin: () -> Unit, onSignup: () -> Unit) {
+    LoginBackground()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LoginTitle()
+
+        Spacer(Modifier.height(32.dp))
+
+        var email = ""
+        EditTextField(
+            value = email,
+            onValueChange = { email = it },
+            placeholder = @Composable {
+                Text(stringResource(id = R.string.email_address))
+            }
+        )
+
+        Spacer(Modifier.height(8.dp))
+
+        var password = ""
+        EditTextField(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = @Composable {
+                Text(stringResource(id = R.string.password))
+            },
+            visualTransformation = PasswordVisualTransformation(),
+        )
+
+        Spacer(Modifier.height(8.dp))
+
+        BigButton(
+            onClick = onLogin,
+            backgroundColor = MaterialTheme.colors.primary
+        ) {
+            Text(stringResource(id = R.string.log_in).toUpperCase(LocaleList.current))
+        }
+        val annotatedString = buildAnnotatedString {
+            append(stringResource(id = R.string.missing_account))
+            append(" ")
+            pushStyle(SpanStyle(textDecoration = TextDecoration.Underline))
+            pushStringAnnotation(tag = "URL", annotation = "signup")
+            append(stringResource(id = R.string.sign_up))
+            pop()
+            pop()
+        }
+
+        ClickableText(
+            text = annotatedString,
+            style = MaterialTheme.typography.body1.copy(color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)),
+            modifier = Modifier.paddingFromBaseline(top = 32.dp),
+            onClick = { offset ->
+                annotatedString
+                    .getStringAnnotations("URL", offset, offset)
+                    .firstOrNull()?.let {
+                        onSignup()
+                    }
+            }
+        )
+    }
+}
+
+@Composable
+fun LoginBackground(){
+    Image(
+        painter = painterResource(id = R.drawable.ic_login),
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colors.background),
+        contentScale = ContentScale.FillBounds
+    )
+}
+
+@Composable
+fun LoginTitle(){
+    Text(
+        text = stringResource(id = R.string.log_in).toUpperCase(LocaleList.current),
+        style = MaterialTheme.typography.h1,
+        modifier = Modifier.paddingFromBaseline(top = 200.dp)
+    )
+}
+
+@Composable
+fun EditTextField (
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier
+            .height(56.dp)
+            .fillMaxWidth(),
+        placeholder = placeholder,
+        leadingIcon = leadingIcon,
+        visualTransformation = visualTransformation,
+        singleLine = true,
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        )
+    )
+}
+
+// endregion
