d25858928a0f52c7d3c985c0f0955749c2557583
yalematta <layale.matta@gmail.com>
2021-03-13 21:54:41 +0200

    implement content of the home screen

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -7,12 +7,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -229,7 +237,7 @@ fun LoginPage(onLogin: () -> Unit, onSignup: () -> Unit) {
 }
 
 @Composable
-fun LoginBackground(){
+fun LoginBackground() {
     Image(
         painter = painterResource(id = R.drawable.ic_login),
         contentDescription = null,
@@ -241,7 +249,7 @@ fun LoginBackground(){
 }
 
 @Composable
-fun LoginTitle(){
+fun LoginTitle() {
     Text(
         text = stringResource(id = R.string.log_in).toUpperCase(LocaleList.current),
         style = MaterialTheme.typography.h1,
@@ -250,7 +258,7 @@ fun LoginTitle(){
 }
 
 @Composable
-fun EditTextField (
+fun EditTextField(
     value: String,
     onValueChange: (String) -> Unit,
     placeholder: @Composable () -> Unit,
@@ -271,6 +279,290 @@ fun EditTextField (
         colors = TextFieldDefaults.textFieldColors(
             backgroundColor = MaterialTheme.colors.surface
         )
+    )
+}
+
+// endregion
+
+// region Home Screen
+
+@Preview(showBackground = true)
+@Composable
+fun HomePagePreview() {
+    MyTheme {
+        Surface(Modifier.size(width = 360.dp, height = 640.dp)) {
+            HomePage()
+        }
+    }
+}
+
+@Composable
+fun HomePage() {
+    Surface(color = MaterialTheme.colors.background) {
+        LazyColumn {
+            item {
+                SearchField()
+            }
+            item {
+                RowTitle(title = stringResource(id = R.string.favorite_collections))
+                RowFavorite()
+            }
+            item {
+                RowTitle(title = stringResource(id = R.string.align_your_body))
+                RowBody()
+            }
+            item {
+                RowTitle(title = stringResource(id = R.string.align_your_mind))
+                RowMind()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CollectionCard(
+    painter: Painter,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.size(width = 192.dp, height = 56.dp),
+        shape = MaterialTheme.shapes.small,
+        elevation = 0.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = title,
+                modifier = Modifier.size(56.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = title,
+                modifier = Modifier.padding(horizontal = 16.dp),
+                style = MaterialTheme.typography.h3
+            )
+        }
+    }
+}
+
+@Composable
+fun Course(
+    painter: Painter,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = title,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(RoundedCornerShape(percent = 50)),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = title,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp),
+            style = MaterialTheme.typography.h3
+        )
+    }
+}
+
+@Composable
+fun SearchField() {
+    Spacer(Modifier.height(56.dp))
+    var search = ""
+    EditTextField(
+        value = search,
+        onValueChange = { search = it },
+        placeholder = @Composable {
+            Text(
+                text = stringResource(id = R.string.search),
+                style = MaterialTheme.typography.body1
+            )
+        },
+        modifier = Modifier.padding(horizontal = 16.dp),
+        leadingIcon = @Composable {
+            SmallIcon(resId = R.drawable.ic_search)
+        }
+    )
+}
+
+@Composable
+fun RowTitle(title: String){
+    Text(
+        text = title.toUpperCase(LocaleList.current),
+        style = MaterialTheme.typography.h2,
+        modifier = Modifier
+            .paddingFromBaseline(top = 40.dp)
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 4.dp)
+    )
+}
+
+@Composable
+fun RowFavorite(){
+    LazyRow(
+        contentPadding = PaddingValues(start = 16.dp)
+    ) {
+        item {
+            Column {
+                CollectionCard(
+                    painter = painterResource(id = R.mipmap.ic_nature_meditations),
+                    title = "Short mantras",
+                    modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+                )
+                CollectionCard(
+                    painter = painterResource(id = R.mipmap.ic_nature_meditations),
+                    title = "Nature meditation",
+                    modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+                )
+            }
+        }
+        item {
+            Column {
+                CollectionCard(
+                    painter = painterResource(id = R.mipmap.ic_stress_and_anxiety),
+                    title = "Stress and anxiety",
+                    modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+                )
+                CollectionCard(
+                    painter = painterResource(id = R.mipmap.ic_self_massage),
+                    title = "Self-massage",
+                    modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+                )
+            }
+        }
+        item {
+            Column {
+                CollectionCard(
+                    painter = painterResource(id = R.mipmap.ic_overwhelmed),
+                    title = "Overwhelmed",
+                    modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+                )
+                CollectionCard(
+                    painter = painterResource(id = R.mipmap.ic_nightly_wind_down),
+                    title = "Nightly wind down",
+                    modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun RowBody() {
+    LazyRow(
+        contentPadding = PaddingValues(start = 16.dp)
+    ) {
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_inversions),
+                title = "Inversions",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_quick_yoga),
+                title = "Quick yoga",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_stretching),
+                title = "Stretching",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_tabata),
+                title = "Tabata",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_hiit),
+                title = "HIIT",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_pre_natal_yoga),
+                title = "Pre-natal yoga",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun RowMind() {
+    LazyRow(
+        contentPadding = PaddingValues(start = 16.dp)
+    ) {
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_meditate),
+                title = "Meditate",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_with_kids),
+                title = "With kids",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_aromatherapy),
+                title = "Aromatherapy",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_on_the_go),
+                title = "On the go",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_with_pets),
+                title = "With pets",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+        item {
+            Course(
+                painter = painterResource(id = R.mipmap.ic_high_stress),
+                title = "High stress",
+                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun SmallIcon(resId: Int) {
+    Icon(
+        painter = painterResource(id = resId),
+        contentDescription = stringResource(id = R.string.search),
+        modifier = Modifier.size(18.dp)
     )
 }
 
